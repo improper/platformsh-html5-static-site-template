0fe527fed94d8175115edc78bbcf8d94dacbcc1a
Tyler Mills <tyler@garlik.io>
2022-07-22 08:59:44 -0400

    Add php handler

---
+++ public/404-handler.php
@@ -0,0 +1,13 @@
+<?php
+header("HTTP/1.0 404 Not Found"); //IMPORTANT: we need to return the correct HTTP status code.
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <title>Welcome to Platform.sh</title>
+    </head>
+    <body style="background: #000000; display: flex; flex-direction: column; padding: 20% 20%; align-items: center; font-size: 5rem; font-family: system-ui; color: white; text-transform: uppercase; font-weight: 700;">
+        <p>Uh-oh, couldn't answer this request.</p>
+    </body>
+</html>
+
